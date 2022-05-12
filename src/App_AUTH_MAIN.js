050e5e2caf960e0c9670d1dd1a070467f102c89b
fotomain <foto888999@gmail.com>
2022-05-12 09:45:24 +0300

    f

--- src/App_AUTH_MAIN.js
+++ src/App_AUTH_MAIN.js
@@ -35,6 +35,9 @@ class App extends React.Component {
 
     componentDidMount() {
         console.log('=== componentDidMount'+Date.now())
+
+        //TODO PREFLY TO GET TOKEN !!!!!!!!!!!!!
+
         // period set
         // this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
 
@@ -47,73 +50,6 @@ class App extends React.Component {
         // console.log('=== componentWillUnmount Home'+Date.now())
         // period clear
         // clearInterval(this.interval);
-
-    }
-
-    crud_create_WooCommerce_Order() {
-
-
-        const url_root = "https://antinedoebit.com/"
-
-        var api_WooCommerceAPI = new WooCommerceAPI({
-            url: url_root,
-            // consumerKey:    "8hBZ83mWd9pC",
-            // consumerSecret: "RsDc0CMLDwnb2Qct59StQcO4N9cEEKBLbkC0FXo5NzsunzMP",
-            consumerKey: "ck_3b027d8d4737ef55b5ecdff4f6421116c117120c",
-            consumerSecret: "cs_eed62fa1661ab15d570cdbb2f0cf07bd9b85e129",
-            wp_api: true,
-            // version: 'v2',
-            version: 'wc/v3',
-            queryStringAuth: true
-        });
-
-        const data = {
-            _method: "POST",
-            // payment_method: "bacs",
-            // payment_method_title: "Direct Bank Transfer",
-            // set_paid: true,
-            billing: {
-                first_name: "John",
-                last_name: "Doe",
-                // address_1: "969 Market",
-                // address_2: "",
-                // city: "San Francisco",
-                // state: "CA",
-                // postcode: "94103",
-                // country: "US",
-                email: "john.doe@example.com",
-                phone: "(555) 555-5555"
-            },
-            // shipping: {
-            //     first_name: "John",
-            //     last_name: "Doe",
-            //     address_1: "969 Market",
-            //     address_2: "",
-            //     city: "San Francisco",
-            //     state: "CA",
-            //     postcode: "94103",
-            //     country: "US"
-            // },
-            line_items: [
-                {
-                    product_id: 11303,
-                    quantity: 2
-                }
-            ],
-            shipping_lines: [
-                {
-                    method_id: "flat_rate",
-                    method_title: "Flat Rate",
-                    total: "10.00"
-                }
-            ]
-        };
-
-        const ret1 = api_WooCommerceAPI.post("orders", data)
-
-        console.log("=== orders ret1 ")
-        console.log(ret1)
-        return ret1;
 
     }
 
@@ -136,38 +72,76 @@ class App extends React.Component {
 
 
         const data = {
-                    //=== https://github.com/woocommerce/woocommerce/wiki/Getting-started-with-the-REST-API
-                    _method: "POST",
-                    //Strong!
-                    sku: Date.now().toString(),
-                        // ,
-                    name: "555 Premium Quality " + Date.now().toString(),
-                    type: "simple",
-                    slug: "prod555",
-                    regular_price: "21.99",
-                    description: "Pellentesque habitant morbi tristique senectus",
-                    short_description: "Pellentesque habitant morbi ",
-                }
+            //=== https://github.com/woocommerce/woocommerce/wiki/Getting-started-with-the-REST-API
+            _method: "POST",
+            //Strong!
+            sku: Date.now().toString(),
+            // ,
+            name: "555 Premium Quality " + Date.now().toString(),
+            type: "simple",
+            slug: "prod555",
+            regular_price: "21.99",
+            description: "Pellentesque habitant morbi tristique senectus",
+            short_description: "Pellentesque habitant morbi ",
+        }
 
-                const ret1 = api_WooCommerceAPI.post("products", data)
+        const ret1 = api_WooCommerceAPI.post("products", data)
 
 
-                console.log("=== products ret1 ")
-                console.log(ret1)
-                return ret1;
+        console.log("=== products ret1 ")
+        console.log(ret1)
+        return ret1;
 
-            }
+    }
+
 
 
     doApi_WooCommerce_Order(e) {
+        this.crud_create_WooCommerce_Order()
+    }
 
-        console.log("=== data_json START")
+    crud_create_WooCommerce_Order() {
 
-        const data_json = this.crud_create_WooCommerce_Order();
+        async function postData(url = '', data = {}) {
 
-        console.log(data_json)
+            let headers1 = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYW50aW5lZG9lYml0LmNvbSIsImlhdCI6MTY1MjE5MzY5NywibmJmIjoxNjUyMTkzNjk3LCJleHAiOjE2NTI3OTg0OTcsImRhdGEiOnsidXNlciI6eyJpZCI6IjIifX19.9HKUgbCaEdCpzJia6-FKHSaFA1-yBUlcGPKMWdZDrFY'
+            }
 
-        console.log("=== data_json FINISH")
+            const response = await fetch(url, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: headers1,
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            });
+            return response.json(); // parses JSON response into native JavaScript objects
+        }
+
+        const url_root="https://antinedoebit.com/wp-json/wc/v2/orders"
+        postData(url_root, {
+            "billing": {
+                "first_name": "John " + Date.now().toString(),
+                "last_name": "Doe",
+                "email": "john.doe@example.com",
+                "phone": "(555) 555-5555"
+            },
+            "line_items": [
+                {
+                    "product_id": 11303,
+                    "quantity": 2
+                }
+            ]
+        })
+            .then(data => {
+                console.log("=== data orders");
+                console.log(data);
+            });
+
 
     }
 
@@ -183,7 +157,7 @@ class App extends React.Component {
 
     }
 
-    doApi(e) {
+    doApi_books_fetch(e) {
 
         async function postData(url = '', data = {}) {
 
@@ -220,7 +194,7 @@ class App extends React.Component {
 
     }
 
-    doApi_axios(e) {
+    doApi_books_axios(e) {
 
         const url_root="https://antinedoebit.com/wp-json/wp/v2/books"
 
@@ -257,7 +231,7 @@ class App extends React.Component {
                 <Books/>
 
                 <h3>Add Book</h3>
-                <Button onClick={(e)=>this.doApi(e)} variant={`contained`}>DO API</Button>
+                <Button onClick={(e)=>this.doApi_books_fetch(e)} variant={`contained`}>DO API</Button>
                 <h3>Add Product</h3>
                 <Button onClick={(e)=>this.doApi_WooCommerce_Product(e)} variant={`contained`}>DO API</Button>
                 <h3>Add Order</h3>
